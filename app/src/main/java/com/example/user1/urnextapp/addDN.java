1ab075afc14723d9c249a32be6ae6c4991ebdd2b
gradproject1 <435.gradproject@gmail.com>
2018-03-21 10:14:55 +0300

    admin

--- app/src/main/java/com/example/user1/urnextapp/addDN.java
+++ app/src/main/java/com/example/user1/urnextapp/addDN.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,8 +25,14 @@ import static android.app.Activity.RESULT_OK;
 public class addDN extends Fragment {
 
    private Button mSelectImage;
+   private Button mSelectVideo;
+   private Button mSelectFashion;
+   private Button mSelectBook;
    private StorageReference mStorage;
-   private static final int GALARRY_INTENT = 2;
+   private static final int IMAGE_INTENT = 1;
+   private static final int VIDEO_INTENT = 2;
+   private static final int FASHION_INTENT = 3;
+   private static final int BOOK_INTENT = 4;
    private ProgressDialog mprogressDialog;
 
 
@@ -39,7 +46,12 @@ public class addDN extends Fragment {
 
       mStorage = FirebaseStorage.getInstance().getReference();
       mSelectImage = (Button) PageOne.findViewById(R.id.selectImage);
+      mSelectVideo = (Button) PageOne.findViewById(R.id.selectVideo);
+      mSelectFashion = (Button) PageOne.findViewById(R.id.selectFashion);
+      mSelectBook = (Button) PageOne.findViewById(R.id.selectBook);
       mprogressDialog = new ProgressDialog(getActivity());
+
+
 
       mSelectImage.setOnClickListener(new View.OnClickListener() {
          @Override
@@ -48,20 +60,53 @@ public class addDN extends Fragment {
             //Intent intent = new Intent(Intent.ACTION_PICK);
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(intent, GALARRY_INTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select image"),IMAGE_INTENT);
          }
       });
+////////////////////////////////////////////////////////////////
+      mSelectVideo.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent();
+            //Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("video/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select video"),VIDEO_INTENT);
+         }
+      });
+/////////////////////////////////////////////////////////////////
+      mSelectFashion.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent();
+            //Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select fashion"),FASHION_INTENT);
+         }
+      });
+///////////////////////////////////////////////////////////////////
+      mSelectBook.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Intent intent = new Intent();
+            //Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("pdf/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select book"),BOOK_INTENT);
+         }
+      });
+//////////////////////////////////////////////////////////////////////
 
       return PageOne;
    }
    @Override
    public void onActivityResult(int requestCode, int resultCode, Intent Data){
       super.onActivityResult(requestCode,resultCode,Data);
-      if(requestCode == GALARRY_INTENT && resultCode == RESULT_OK ){
-
+      if(resultCode == RESULT_OK ){
+         if(requestCode == IMAGE_INTENT){
          mprogressDialog.setMessage(" Uploading... ");
          mprogressDialog.show();
-
          Uri uri = Data.getData();
          StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
          filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -72,6 +117,56 @@ public class addDN extends Fragment {
 
             }
          });
+      }
+      else if(requestCode == VIDEO_INTENT){
+
+            mprogressDialog.setMessage(" Uploading... ");
+            mprogressDialog.show();
+
+            Uri uri = Data.getData();
+            StorageReference filepath = mStorage.child("Videos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+               @Override
+               public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                  Toast.makeText(getActivity(),"Upload done",Toast.LENGTH_LONG).show();
+                  mprogressDialog.dismiss();
+
+               }
+            });
+         }
+
+         else if(requestCode == FASHION_INTENT){
+            mprogressDialog.setMessage(" Uploading... ");
+            mprogressDialog.show();
+
+            Uri uri = Data.getData();
+            StorageReference filepath = mStorage.child("Fashion").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+               @Override
+               public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                  Toast.makeText(getActivity(),"Upload done",Toast.LENGTH_LONG).show();
+                  mprogressDialog.dismiss();
+
+               }
+            });
+         }
+
+         else if(requestCode == BOOK_INTENT){
+            mprogressDialog.setMessage(" Uploading... ");
+            mprogressDialog.show();
+
+            Uri uri = Data.getData();
+            StorageReference filepath = mStorage.child("Books").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+               @Override
+               public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                  Toast.makeText(getActivity(),"Upload done",Toast.LENGTH_LONG).show();
+                  mprogressDialog.dismiss();
+
+               }
+            });
+         }
+
       }
    }
 }
